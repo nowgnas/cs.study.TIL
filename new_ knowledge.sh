9ecf6ec51fe9c9e7f5be64446c18338672dba2f0
nowgnas <dev.nowgnas@gmail.com>
2023-06-02 14:48:01 +0900

    :sparkles: Create script file

---
+++ new_ knowledge.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Prompt the user for the folder name
+echo "Enter folder name (uppercase):"
+read folder_name
+
+# Convert folder name to uppercase
+folder_name=$(echo "$folder_name" | tr '[:lower:]' '[:upper:]')
+
+# Check if the folder already exists
+if [ -d "$folder_name" ]; then
+    echo "Folder '$folder_name' already exists."
+else
+    # Create the folder
+    mkdir "$folder_name"
+    echo "Folder '$folder_name' created."
+fi
+
+# Prompt the user for the topic
+echo "Enter the topic:"
+read topic
+
+# Get the current date
+current_date=$(date +%y-%m-%d)
+
+# Create the file name with the specified format
+file_name="$current_date-$topic.md"
+
+# Create the file inside the folder
+touch "$folder_name/$file_name"
+
+# Display a success message
+echo "File '$file_name' created inside folder '$folder_name'."
